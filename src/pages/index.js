f69175a15dc49dba3fd2d1fd9c400a1dc80c7527
강민정 <staccato189@gmail.com>
2023-08-06 18:10:45 +0900

    navigator 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,4 @@
-export { default as Home } from "./Home";
-export { default as Recommend } from "./Recommend";
-export { default as Search } from "./Search";
-export { default as Veiw } from "./Veiw";
-export { default as Signin } from "./Signin";
 export { default as Admin } from "./Admin/Admin";
 export { default as Home } from "./Home/Home";
+export { default as Login } from "./Login/Login";
 export { default as Signup } from "./Signup/Signup";
--- src/routes/Navigator.js
+++ src/routes/Navigator.js
@@ -5,7 +5,7 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
-import { Home, Admin } from "../pages/index";
+import { Home, Admin, Login } from "../pages/index";
 import Signup from "../pages/Signup/Signup";
 
 const router = createBrowserRouter(
@@ -14,6 +14,7 @@ const router = createBrowserRouter(
       <Route path="/" element={<Home />} />
       <Route path="/admin" element={<Admin />} />
       <Route path="/signup/*" element={<Signup />} />
+			<Route path="/login" element={<Login />} />
       <Route path="*" element={<div>없는페이지</div>} />
     </Route>
   )
