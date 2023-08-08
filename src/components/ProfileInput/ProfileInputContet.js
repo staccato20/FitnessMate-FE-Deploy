40246b52509f96e9c3839fa891f114a234443b16
최훈오 <gnsdh86166@gmail.com>
2023-08-08 16:24:06 +0900

    merge

--- src/components/ProfileInput/ProfileInputContet.js
+++ src/components/ProfileInput/ProfileInputContet.js
@@ -3,49 +3,18 @@ import { ProfileInputContentWrapper } from "./StyledProfileInput";
 import { validationState } from "../../recoil/atom";
 import ValidateTest from "./../../utils/exp";
 
-const ProfileInputContent = ({ placeholder, name }) => {
-  const [isClicked, setIsClicked] = useState(false);
+const ProfileInputContent = ({
+  placeholder,
+  name,
+  isFocused,
+  setIsFocused,
+}) => {
+  const [isValidState, setIsValidState] = useRecoilState(validationState);
 
-  // 나중에 utils로 빼기
-  const usernameExp = /^[가-힣]{2,10}$/;
-  const passwordExp = /^[a-zA-Z0-9]{8,}$/;
-  const birthdateExp = /^\d{4}-\d{2}-\d{2}$/;
-
-  const [isValidateChecked, setIsValidateChecked] =
-    useRecoilState(validationState);
-
-  const [isNextButton, setIsNextButton] = useRecoilState(nextButtonValidate);
-  // 비밀번호 재 확인
-
-  const Validation = (name) => {
-    return;
-  };
-
-  // 유효성 검사
   const handleChange = (e) => {
     const value = e.currentTarget.value;
     const name = e.target.name;
-    let exp = Validation(name);
-    switch (name) {
-      case "username":
-        exp = usernameExp;
-        break;
-
-      case "birthdate":
-        exp = birthdateExp;
-        break;
-
-      case "password":
-        exp = passwordExp;
-        break;
-
-      case "password2":
-        exp = passwordExp;
-        break;
-
-      default:
-        break;
-    }
+    let exp = ValidateTest(name);
 
     // 유효성 검사
     const updatedValidationState = {
@@ -54,34 +23,19 @@ const ProfileInputContent = ({ placeholder, name }) => {
     };
     setIsValidState(updatedValidationState);
 
-    // 이메일은 프론트가 아닌 백에서 검사(중복 + 유효성검사), 값만 업데이트 함
-    const updateEmailState = {
-      ...isValidateChecked,
-      [name]: [value, isValidateChecked[name][1], isValidateChecked[name][2]],
-    };
-
-    if (name === "email") {
-      setIsValidateChecked(updateEmailState);
-    } else {
-      setIsValidateChecked(updatedValidationState);
-    }
-
-    // 패스워드가 바뀔때만 검사
+    const passwordSame =
+      updatedValidationState.password[0] ===
+      updatedValidationState.password2[0];
+    // 비밀번호 재확인(value는 그대로, 재확인만 함)
     if (name.includes("password")) {
-      if (
-        updatedValidationState.password[0] ===
-        updatedValidationState.password2[0]
-      ) {
-        setIsValidateChecked((pre) => ({
-          ...pre,
-          password2: [value, true, updatedValidationState[name][2]],
-        }));
-      } else {
-        setIsValidateChecked((pre) => ({
-          ...pre,
-          password2: [value, false, updatedValidationState[name][2]],
-        }));
-      }
+      setIsValidState((pre) => ({
+        ...pre,
+        password2: [
+          updatedValidationState.password2[0],
+          passwordSame,
+          updatedValidationState[name][2],
+        ],
+      }));
     }
   };
 
