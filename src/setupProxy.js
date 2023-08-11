c39901dea6b086a7a6dfd7d5842f053d7099b1fd
최훈오 <gnsdh86166@gmail.com>
2023-08-11 16:13:55 +0900

    refactor: 환경 변수 제거

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,8 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const TARGET = process.env.REACT_APP_DOMAIN;
 module.exports = (app) => {
   app.use(
     createProxyMiddleware("/api", {
-      target: TARGET,
+      target: "http://43.200.20.25:8080",
       changeOrigin: true,
       pathRewrite: { "^/api": "" },
     })
