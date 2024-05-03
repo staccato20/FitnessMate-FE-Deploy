f83de1ce4abf41ca8b5c1636e1e2754605c9b43e
Whoknow77 <gnsdh86166@gmail.com>
2024-05-03 16:16:12 +0900

    [Test] 배포 테스트

--- src/pages/MyPage/Fix/FixProfile.js
+++ src/pages/MyPage/Fix/FixProfile.js
@@ -1,89 +1,86 @@
-import React, { useCallback, useEffect, useState } from "react";
-import * as S from "./StyledFix";
-import { useRecoilValue } from "recoil";
-import { useNavigate } from "react-router-dom";
-import { validationState } from "./../../../recoil/atom";
-import { userPutAPI } from "../../../apis/API";
-import ProfileInput from "./ProfileInput/ProfileInput";
-import TokenApi from "../../../apis/TokenApi";
-import ValidateTest from "../../../utils/exp";
-import { useRecoilState } from "recoil";
+import React, {useCallback, useEffect, useState} from "react"
+import * as S from "./StyledFix"
+import {useRecoilValue} from "recoil"
+import {useNavigate} from "react-router-dom"
+import {validationState} from "./../../../recoil/atom"
+import {userPutAPI} from "../../../apis/API"
+import ProfileInput from "./ProfileInput/ProfileInput"
+import TokenApi from "../../../apis/TokenApi"
+import ValidateTest from "../../../utils/exp"
+import {useRecoilState} from "recoil"
 
 // 회원가입 페이지에 대한 정보를 모두 담는 컴포넌트
 // 우선 이메일은
 const FixProfile = (props) => {
+	const navigate = useNavigate()
 
-	const navigate = useNavigate();
-
-	const [name, setuserName] = useState("");
-	const [birth, setbirthDate] = useState("");
-	const [email, setloginEmail] = useState("");
+	const [name, setuserName] = useState("")
+	const [birth, setbirthDate] = useState("")
+	const [email, setloginEmail] = useState("")
 
 	const handleBackPage = (e) => {
-		e.preventDefault();
-		navigate(-1);
-	};
+		e.preventDefault()
+		navigate(-1)
+	}
 
 	// input onchange
 
 	// 유효성 검사
-	const [isValidState, setIsValidState] = useRecoilState(validationState);
+	const [isValidState, setIsValidState] = useRecoilState(validationState)
 	// 포커스 검사
-	const [isFocused, setIsFocused] = useState(false);
+	const [isFocused, setIsFocused] = useState(false)
 
 	// 입력했는지 체크(한 번 입력한 순간 쭉 true)
-	const [valueHistory, setValueHistory] = useState(false);
-
+	const [valueHistory, setValueHistory] = useState(false)
 
 	const handleChange = (e) => {
-		const name = e.target.name;
+		const name = e.target.name
 
 		if (name === "userName") {
-			setuserName(e.target.value);
+			setuserName(e.target.value)
 		} else if (name === "birthDate") {
-			setbirthDate(e.target.value);
+			setbirthDate(e.target.value)
 		}
 		if (!valueHistory) {
-			setValueHistory(true);
+			setValueHistory(true)
 		}
-	};
+	}
 
 	// 제출
 	const handleSubmit = async (e) => {
-		e.preventDefault();
+		e.preventDefault()
 
 		const formData = {
 			userName: name,
 			birthDate: birth,
-		};
-		console.log("정보:", formData);
+		}
+		console.log("정보:", formData)
 
 		try {
 			// API 호출 및 form 데이터 전송
-			const res = await TokenApi.post("user/private", formData);
-			console.log("수정:", res.status);
+			const res = await TokenApi.post("user/private", formData)
+			console.log("수정:", res.status)
 			window.location.replace("fixprofile")
 		} catch (error) {
-			console.log(error);
-			alert("수정 실패. 형식을 준수해주세요.");
+			console.log(error)
+			alert("수정 실패. 형식을 준수해주세요.")
 		}
-	};
-
+	}
 
 	const fetchData = async () => {
 		try {
-			const response = await TokenApi.get("user/private");
-			setuserName(response.data.userName);
-			setbirthDate(response.data.birthDate);
-			setloginEmail(response.data.loginEmail);
+			const response = await TokenApi.get("user/private")
+			setuserName(response.data.userName)
+			setbirthDate(response.data.birthDate)
+			setloginEmail(response.data.loginEmail)
 		} catch (error) {
-			console.log(error);
+			console.log(error)
 		}
-	};
+	}
 
 	useEffect(() => {
-		fetchData();
-	}, []); // 빈 의존성 배열을 추가하여 이 효과가 한 번만 실행되도록
+		fetchData()
+	}, []) // 빈 의존성 배열을 추가하여 이 효과가 한 번만 실행되도록
 
 	return (
 		<S.SignupContainer>
@@ -92,19 +89,17 @@ const FixProfile = (props) => {
 			</S.SignupTitle>
 			<S.ProfileInputcontainer>
 				<ProfileInput
-					placeholder="2자리 이상"
+					placeholder="3자리 이상"
 					name="userName"
 					value={name}
-					handleChange={handleChange}
-				>
+					handleChange={handleChange}>
 					이름
 				</ProfileInput>
 				<ProfileInput
 					placeholder="YYYY-MM-DD"
 					name="birthDate"
 					value={birth}
-					handleChange={handleChange}
-				>
+					handleChange={handleChange}>
 					생년월일
 				</ProfileInput>
 				<S.NonFix>
@@ -112,13 +107,22 @@ const FixProfile = (props) => {
 					<div className="nonfix-content">{email}</div>
 				</S.NonFix>
 			</S.ProfileInputcontainer>
-			<S.FixPassword onClick={() => { navigate("../fixpassword"); }}>비밀번호 변경하기</S.FixPassword>
+			<S.FixPassword
+				onClick={() => {
+					navigate("../fixpassword")
+				}}>
+				비밀번호 변경하기
+			</S.FixPassword>
 			<S.ButtonContainer>
 				<S.CancelButton onClick={handleBackPage}>취소</S.CancelButton>
-				<S.SaveButton onClick={handleSubmit} type="submit">변경 사항 저장하기</S.SaveButton>
+				<S.SaveButton
+					onClick={handleSubmit}
+					type="submit">
+					변경 사항 저장하기
+				</S.SaveButton>
 			</S.ButtonContainer>
 		</S.SignupContainer>
-	);
-};
+	)
+}
 
-export default FixProfile;
+export default FixProfile
