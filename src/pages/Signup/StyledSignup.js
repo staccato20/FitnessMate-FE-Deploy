719a42ca85be2315661468b2d8328572c2c60e8f
최훈오 <gnsdh86166@gmail.com>
2024-06-01 19:15:28 +0900

    [Remove] 불필요한 속성 제거

--- src/pages/Signup/StyledSignup.js
+++ src/pages/Signup/StyledSignup.js
@@ -70,12 +70,6 @@ export const SignupContainer = styled.form`
 	}
 `
 
-export const SignupTitleContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 12px;
-`
-
 export const SignupTitle = styled.span`
 	text-align: ${({flex}) => (flex ? "center" : "left")};
 	display: ${({flex}) => (flex ? "flex" : "")};
@@ -86,24 +80,6 @@ export const SignupTitle = styled.span`
 	font-size: 30px;
 	font-weight: 600;
 	letter-spacing: -0.6px;
-	.warningNoWrite {
-		display: inline-block;
-		color: ${theme.Gray80};
-		font-size: 22px;
-		padding-top: 12px;
-	}
-	.signupCompleteTitle {
-		color: ${theme.Gray80};
-		font-size: 22px;
-		font-weight: 600;
-		letter-spacing: -0.44px;
-	}
-`
-
-export const TitleEmphasis = styled.span`
-	color: ${theme.Black};
-	font-size: 28px;
-	font-weight: 700;
 `
 
 export const BodyInfoContainer = styled.div`
@@ -112,20 +88,6 @@ export const BodyInfoContainer = styled.div`
 	align-items: flex-start;
 	gap: 24px;
 	width: 100%;
-	.sexSelect {
-		display: flex;
-		flex-direction: column;
-		gap: 8px;
-		.bodyInfoWarning {
-			color: ${theme.Error};
-			font-size: 16px;
-		}
-	}
-
-	.sexList {
-		display: flex;
-		gap: 5px;
-	}
 `
 
 export const ProfileInputcontainer = styled.form`
@@ -133,41 +95,6 @@ export const ProfileInputcontainer = styled.form`
 	flex-direction: column;
 	gap: 12px;
 	width: 100%;
-`
-export const SexName = styled.span`
-	transition: all 0.3s ease-out;
-	color: ${({sex}) => (sex ? theme.BrandDark : theme.Gray50)};
-	font-size: 16px;
-	font-weight: 600;
-	letter-spacing: -0.32px;
-`
-
-export const SexItem = styled.button`
-	transition: all 0.3s ease-out;
-	border-radius: 16px;
-	background: ${({sex}) => (sex ? theme.Gray10 : theme.White)};
-	border: ${({sex, theme}) =>
-		sex ? `2px solid ${theme.Brand}` : `2px solid transparent`};
-	display: flex;
-	width: 126px;
-	height: 126px;
-	padding: 18px 32px 19px 31px;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-	gap: 8px;
-	.sexImg {
-		width: 60px;
-		height: 60px;
-	}
-	&:hover {
-		background: ${theme.Gray10};
-		border: 2px solid
-			${({sex}) => (sex ? `2px solid ${theme.Brand}` : theme.Gray30)};
-	}
-	&:hover ${SexName} {
-		color: ${({sex}) => (sex ? theme.BrandDark : theme.Black)};
-	}
 `
 
 export const SignupTextContainer = styled.div`
@@ -177,11 +104,6 @@ export const SignupTextContainer = styled.div`
 	width: 100%;
 `
 
-export const BodyCompositionInputList = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 24px;
-`
 export const ButtonContainer = styled.div`
 	display: flex;
 	align-items: center;
