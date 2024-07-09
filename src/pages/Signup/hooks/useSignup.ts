8d3e6aef758b477638ee3664dd9efcbe6ee17470
강민정 <80515968+staccato20@users.noreply.github.com>
2024-07-09 18:25:57 +0900

    Update useSignup.ts

--- src/pages/Signup/hooks/useSignup.ts
+++ src/pages/Signup/hooks/useSignup.ts
@@ -1,23 +1,19 @@
-// @ts-nocheck
-
 import { Message } from "@/types"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
-
 import { useParams } from "react-router-dom"
 import sendMessageAPI from "../apis/sendMessageAPI"
 import { QUERY_KEY_GET_GROUP_MESSAGE_LIST } from "./useMessageGroupList"
 import { QUERY_KEY_GET_MESSAGE_LIST } from "./useMessageList"
-
-import { useModal } from "@/hooks/useModal" // useModal 훅을 import 합니다.
-import AlertModal from "@/components/AlertModal" // AlertModal 컴포넌트를 import 합니다.
-import { DM_ERROR } from "@/constants/errors" // DM_ERROR 상수를 import 합니다.
-import sendNotification from "@/utils/sendNotification" // sendNotification 함수를 import 합니다.
+import { useModal } from "@/hooks/useModal"
+import AlertModal from "@/components/AlertModal"
+import { DM_ERROR } from "@/constants/errors"
+import sendNotification from "@/utils/sendNotification"
 
 export const QUERY_KEY_SEND_MESSAGE = "SEND_MESSAGE"
 
 const useSendMessage = () => {
   const queryClient = useQueryClient()
-  const { userId: othersUserId } = useParams()
+  const { userId: othersUserId } = useParams<{ userId: string }>()
   const { isShowModal, showModal, closeModal } = useModal()
 
   const AlertModalComponent = isShowModal ? (
@@ -28,10 +24,10 @@ const useSendMessage = () => {
     />
   ) : null
 
-  const sendMessage = useMutation({
+  const sendMessage = useMutation<Message, unknown, { message: string }>({
     mutationKey: [QUERY_KEY_SEND_MESSAGE],
     mutationFn: sendMessageAPI,
-    onSuccess: (response: Message) => {
+    onSuccess: (response) => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEY_GET_MESSAGE_LIST] })
       if (!othersUserId) {
         return
@@ -51,6 +47,7 @@ const useSendMessage = () => {
       showModal()
     },
   })
+
   return { AlertModalComponent, sendMessage }
 }
 
