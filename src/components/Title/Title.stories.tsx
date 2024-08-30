a7f7930d33ab8c2fac1bebc4102e41d7b75d7eba
최훈오 <gnsdh86166@gmail.com>
2024-08-30 20:58:05 +0900

    [Feat ✏️] Top 컴포넌트 추가

---
+++ src/components/Title/Title.stories.tsx
@@ -0,0 +1,67 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import Title from "@components/Title/Title"
+
+import Variant from "./Title"
+
+const meta: Meta<typeof Title> = {
+  title: "components/Title",
+  component: Title,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+    componentSubtitle: `Title 가이드를 제공해주는 컴포넌트입니다.`,
+    docs: {
+      description: {
+        component: `
+  - 다음과 같은 컴포넌트를 children으로 사용할 수 있습니다.
+        - <Title.TopSubTtile> 상단의 텍스트를 나타냅니다.
+        - <Title.BottomSubTitle> 하단의 텍스트를 나타냅니다.
+        `,
+      },
+    },
+
+    argTypes: {
+      variant: {
+        control: {
+          type: "radio",
+        },
+        options: typeof Variant,
+      },
+    },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof Title>
+
+export const Primary: Story = {
+  render: () => <Title variant="big">메인 타이틀</Title>,
+}
+export const WithTop: Story = {
+  render: () => (
+    <Title variant="big">
+      <Title.SubTopTitle>보조 타이틀</Title.SubTopTitle>
+      메인 타이틀
+    </Title>
+  ),
+}
+
+export const WithBottom: Story = {
+  render: () => (
+    <Title variant="mid">
+      메인 타이틀
+      <Title.SubBottomTitle>보조 타이틀</Title.SubBottomTitle>
+    </Title>
+  ),
+}
+
+export const WithTopBottom: Story = {
+  render: () => (
+    <Title variant="small">
+      <Title.SubTopTitle>보조 타이틀</Title.SubTopTitle>
+      메인 타이틀
+      <Title.SubBottomTitle>보조 타이틀</Title.SubBottomTitle>
+    </Title>
+  ),
+}
