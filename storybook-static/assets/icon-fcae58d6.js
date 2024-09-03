00e6af706a11a6f067d8259ca62b20cef28b7909
강민정 <staccato189@gmail.com>
2024-09-03 21:13:57 +0900

    [Fix 🔨] 스토리북 빌드 수정 및 빌드 결과물 ignore

--- storybook-static/assets/icon-fcae58d6.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-2aae9559.js";import{u as i}from"./index-571eae1e.js";import{ae as m,af as a,ag as c}from"./index-ee803c5f.js";import{I as r,a as p}from"./Icon-d5118378.js";import"./index-ff614419.js";import"./iframe-57a275b8.js";import"../sb-preview/runtime.js";import"./index-4da86cf0.js";import"./index-1b441bc2.js";import"./index-db52ff5b.js";import"./index-356e4a49.js";function s(n){const e={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{title:"Getting Started/Icon"}),`
-`,t.jsx(e.h1,{id:"icons",children:"Icons"}),`
-`,t.jsxs(e.p,{children:["there are ",Object.keys(r).length," icons"]}),`
-`,t.jsx("br",{}),`
-`,t.jsx(a,{style:{marginTop:"50px"},children:Object.keys(r).map(o=>t.jsx(c,{name:o,children:t.jsx(p,{icon:o})}))})]})}function C(n={}){const{wrapper:e}={...i(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(s,{...n})}):s(n)}export{C as default};
--- storybook-static/assets/pallete-f0781b74.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./jsx-runtime-2aae9559.js";import{u as o}from"./index-571eae1e.js";import{ae as l,ah as a,ai as n}from"./index-ee803c5f.js";import"./index-ff614419.js";import"./iframe-57a275b8.js";import"../sb-preview/runtime.js";import"./index-4da86cf0.js";import"./index-1b441bc2.js";import"./index-db52ff5b.js";import"./index-356e4a49.js";function s(e){const t={h1:"h1",h2:"h2",...o(),...e.components};return r.jsxs(r.Fragment,{children:[r.jsx(l,{title:"Getting Started/Palette"}),`
-`,r.jsx(t.h1,{id:"colors",children:"Colors"}),`
-`,r.jsx("br",{}),`
-`,r.jsx(t.h2,{id:"brand",children:"Brand"}),`
-`,r.jsxs(a,{children:[r.jsx(n,{title:"Brand50~500",colors:{Brand50:"#EBF7FF",Brand100:"#E0F2FF",Brand150:"#DDEAF4",Brand200:"#D2ECFF",Brand300:"#99D4FF",Brand400:"#6CC1FE",Brand500:"#3EAEFF"}}),r.jsx(n,{title:"Brand600~990",colors:{Brand600:"#0B98FF",Brand700:"#048EF1",Brand750:"#0F84D9",Brand800:"#0672BF",Brand900:"#035896",Brand950:"#01375E",Brand990:"#042741"}})]}),`
-`,r.jsx(t.h2,{id:"grayscale",children:"Grayscale"}),`
-`,r.jsxs(a,{children:[r.jsx(n,{title:"Neutral0~450",colors:{Neutral0:"#FFFFFF",Neutral50:"#FBFCFC",Neutral100:"#F9FAFB",Neutral200:"#F2F4F6",Neutral300:"#E4EAF0",Neutral400:"#D0D9E2",Neutral450:"#BBC8D6"}}),r.jsx(n,{title:"Neutral600~990",colors:{Neutral500:"#A2B2C2",Neutral600:"#6B7684",Neutral700:"#626D7D",Neutral800:"#4E5968",Neutral900:"#333D4B",Neutral950:"#272E38",Neutral990:"#191F28"}})]}),`
-`,r.jsx(t.h2,{id:"system",children:"System"}),`
-`,r.jsx(a,{children:r.jsx(n,{title:"System",colors:{Error:"#EB444C",Success:"#2EDD6A"}})})]})}function E(e={}){const{wrapper:t}={...o(),...e.components};return t?r.jsx(t,{...e,children:r.jsx(s,{...e})}):s(e)}export{E as default};
