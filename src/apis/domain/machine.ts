7666155e7dff0519cfb1fbe98e77393de3459b49
최훈오 <gnsdh86166@gmail.com>
2024-09-11 21:36:28 +0900

    [Refactoring ⚙️] 체크박스 관리를 Set자료구조로 이용

--- src/apis/domain/machine.ts
+++ src/apis/domain/machine.ts
@@ -2,8 +2,13 @@ import { instance } from "@apis/instance"
 
 import { MachineList, PostMachineListPayload } from "@typpes/type"
 
-const fetchList = (bodyPartList: PostMachineListPayload) =>
-  instance.post<MachineList[]>("/api/machines/list", bodyPartList)
+const fetchList = async (bodyPartList: PostMachineListPayload) => {
+  const response = await instance.post<MachineList[]>(
+    "/api/machines/list",
+    bodyPartList,
+  )
+  return response.data
+}
 
 const machineAPI = { fetchList }
 export default machineAPI
