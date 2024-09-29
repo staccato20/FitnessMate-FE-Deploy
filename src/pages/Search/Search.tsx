407018dc9bcea6cdb654293c9089a89546fe8892
최훈오 <gnsdh86166@gmail.com>
2024-09-29 22:55:08 +0900

    [Refactoring ⚙️] Search 수정

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,21 +1,5 @@
-import { Route, Routes } from "react-router-dom"
-
-import SearchHome from "@pages/Search/SearchHome/SearchHome"
-import SearchWorkoutDetail from "@pages/Search/SearchWorkoutDetail/SearchWorkoutDetail"
-
 const Search = () => {
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={<SearchHome />}
-      />
-      <Route
-        path="workoutdetail"
-        element={<SearchWorkoutDetail />}
-      />
-    </Routes>
-  )
+  return <div>gg</div>
 }
 
 export default Search
