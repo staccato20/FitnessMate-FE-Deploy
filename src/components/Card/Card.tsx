0743975bc1b187c24679a46344c88114b1d8a4d3
최훈오 <gnsdh86166@gmail.com>
2024-10-01 12:42:47 +0900

    [Feat ✏️] 카드 컴포넌트 제작

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,3 +1,32 @@
-const Card = () => {}
+import { ButtonHTMLAttributes } from "react"
+
+import Badge from "@components/Badge/Badge"
+
+import * as S from "./StyledCard"
+
+interface CardProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  src: string
+  title: string
+  badges: string[]
+}
+
+const Card = ({ src, title, badges }: CardProps) => {
+  return (
+    <S.CardWrapper>
+      <S.CardImg
+        src={src}
+        alt="운동 이미지"
+      />
+      <S.CardContents>
+        <S.CardText>{title}</S.CardText>
+        <S.BadgeList>
+          {badges.map((badge) => (
+            <Badge key={badge}>{badge}</Badge>
+          ))}
+        </S.BadgeList>
+      </S.CardContents>
+    </S.CardWrapper>
+  )
+}
 
 export default Card
