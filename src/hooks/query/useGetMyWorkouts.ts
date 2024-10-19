304c93536dbc7f50bebf33eef44031c21c4248e1
최훈오 <gnsdh86166@gmail.com>
2024-10-19 15:43:19 +0900

    [Fix 🪛] useGetMyWorkouts 타입 오류 수정

--- src/hooks/query/useGetMyWorkouts.ts
+++ src/hooks/query/useGetMyWorkouts.ts
@@ -4,15 +4,12 @@ import MyFitAPI from "../../apis/domain/myfit"
 import { MyWorkoutList } from "../../types/type"
 
 // enabled 옵션을 내부에서 처리
-export const useGetMyWorkouts = (routineId: number | null) => {
+export const useGetMyWorkouts = (routineId: number) => {
   return useQuery<MyWorkoutList[]>({
     queryKey: ["getMyWorkouts", routineId],
     queryFn: async () => {
-      if (routineId) {
-        const response = await MyFitAPI.myWorkout(routineId)
-        return Array.isArray(response.data) ? response.data : []
-      }
-      return []
+      const response = await MyFitAPI.myWorkouts(routineId)
+      return Array.isArray(response) ? response : []
     },
     enabled: !!routineId, // routineId가 있을 때만 실행
   })
