feaab0ee5649e69b969ef5467a1dac5965ccc859
최훈오 <gnsdh86166@gmail.com>
2024-11-25 17:24:06 +0900

    [Refactoring ⚙️] 로그아웃 훅 생성

--- src/components/Navbar/NavDropDown.tsx
+++ src/components/Navbar/NavDropDown.tsx
@@ -1,9 +1,6 @@
-import { useNavigate } from "react-router-dom"
-
 import Button from "@components/Button/Button"
 
-import authAPI from "@apis/domain/auth"
-
+import { useLogout } from "@hooks/mutation/useLogout"
 import { useDropDown } from "@hooks/useDropDown"
 
 import * as S from "./StyledNavModal"
@@ -13,21 +10,8 @@ interface NavDropDownProps {
 }
 
 const NavDropDown = ({ userName }: NavDropDownProps) => {
-  const navigate = useNavigate()
   const { isOpen, dropDownRef, toggleDropDown } = useDropDown()
-
-  const handleLogout = async () => {
-    try {
-      await authAPI.logout()
-      localStorage.clear()
-      navigate("/")
-      window.location.reload()
-    } catch (err) {
-      localStorage.clear()
-      navigate("/")
-      window.location.reload()
-    }
-  }
+  const { mutate: logout } = useLogout()
 
   const handleFixProfile = async () => {
     alert("현재 수정중인 페이지입니다!")
@@ -54,7 +38,7 @@ const NavDropDown = ({ userName }: NavDropDownProps) => {
           <S.NavDropDownList>
             <Button
               variant="text"
-              onClick={handleLogout}>
+              onClick={() => logout()}>
               로그아웃
             </Button>
             <Button
