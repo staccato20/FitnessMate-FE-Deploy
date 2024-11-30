63e8f35ca65a93ace3fa17ac88dd972977c11669
최훈오 <gnsdh86166@gmail.com>
2024-11-30 14:14:15 +0900

    [Refactoring ⚙️] scrollTop 함수간소화

--- src/components/ScrollTop/ScrollTop.tsx
+++ src/components/ScrollTop/ScrollTop.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react"
 import { useLocation } from "react-router-dom"
 
-export default function ScrollToTop() {
+export const ScrollToTop = () => {
   const { pathname } = useLocation()
 
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+    window.scrollTo(0, 0)
   }, [pathname])
 
   return null
