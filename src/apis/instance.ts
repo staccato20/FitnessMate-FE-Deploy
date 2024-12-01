b364e3c79e2fce64cab12e8bc7ee0e9276a99154
최훈오 <gnsdh86166@gmail.com>
2024-12-01 19:05:08 +0900

    [Refactoring ⚙️] console.log -> console.error 로변경

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -12,7 +12,6 @@ instance.interceptors.request.use((config) => {
   if (config.url === "/api/auth/refresh" || config.url === "/api/auth/logout") {
     const refreshToken = localStorage.getItem("refreshToken")
     if (refreshToken) {
-      console.log("리프레쉬 토큰 보냄")
       config.headers.Authorization = `Bearer ${refreshToken}`
     }
   } else {
@@ -30,14 +29,14 @@ instance.interceptors.response.use(
   },
   async (error) => {
     if (error.response.data.status === "ROUTINE_NOT_FOUND_EXCEPTION") {
-      console.log("routineId와 일치하는 routine이 없습니다")
+      console.error("routineId와 일치하는 routine이 없습니다")
     }
     if (error.response.data.status === "ALREADY_EXIST_MY_WORKOUT_EXCEPTION") {
-      console.log("이미 존재하는 운동입니다")
+      console.error("이미 존재하는 운동입니다")
     }
 
     if (error.response.data.status === "ALREADY_LOGOUT_EXCEPTION") {
-      console.log("이미 로그인 되었음")
+      console.error("이미 로그인 되었음")
     }
 
     if (error.response.data.status === "EXPIRED_ACCESS_TOKEN_EXCEPTION") {
@@ -56,7 +55,7 @@ instance.interceptors.response.use(
           return await axios(originalRequest)
         }
       } catch (err) {
-        console.log(err)
+        console.error(err)
       }
     }
 
