08db212ede96dd0876169b4a67c71b66cda056ca
staccato20 <staccato20@users.noreply.github.com>
2024-12-01 18:47:14 +0000

    Update from Frontend repo by staccato20

--- src/hooks/query/useGetMyRoutines.ts
+++ src/hooks/query/useGetMyRoutines.ts
@@ -3,13 +3,8 @@ import { useQuery } from "@tanstack/react-query"
 import MyFitAPI from "../../apis/domain/myfit"
 
 export const useGetMyRoutines = () => {
-  const { data: myRoutines = [] } = useQuery({
+  return useQuery({
     queryKey: ["myRoutines"],
-    queryFn: async () => {
-      const response = await MyFitAPI.myRoutines()
-      return Array.isArray(response.data) ? response.data : []
-    },
+    queryFn: async () => await MyFitAPI.myRoutines(),
   })
-
-  return { myRoutines }
 }
