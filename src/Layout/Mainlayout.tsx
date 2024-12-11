3189fa9f598e750f4094759bfa5728fd90c048c9
최훈오 <gnsdh86166@gmail.com>
2024-12-11 19:07:18 +0900

    [Refactoring ⚙️] usePathStore 삭제

--- src/Layout/Mainlayout.tsx
+++ src/Layout/Mainlayout.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react"
 import { Outlet, useLocation } from "react-router-dom"
 
-import { usePathStore } from "@store/usePathStore"
 import { useUserStore } from "@store/useUserStore"
 
 import Footer from "@components/Footer/Footer"
@@ -15,13 +14,11 @@ const MainLayout = () => {
   const isLogin = path.includes("login")
   const isSignup = path.includes("signup")
   const isMyPage = path.includes("mypage")
-  const { setIsRecommendPage } = usePathStore()
   const { checkLogin } = useUserStore()
 
   useEffect(() => {
-    setIsRecommendPage(path)
     checkLogin()
-  }, [location, path, setIsRecommendPage, checkLogin])
+  }, [location, path, checkLogin])
 
   return (
     <>
